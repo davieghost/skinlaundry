a8113fb289b59471dd8f1bac1566e8e821de43a2
davieghost <davidjohnson1297@gmail.com>
2015-02-11 14:14:57 -0800

    initial commit

---
+++ source/javascripts/application.js
@@ -0,0 +1,28 @@
+//= require_tree .
+
+$("#nav-open").click(function(e) {
+    //sniff for iOS
+    var $np = $('#nav-page');
+    e.stopPropagation();
+    
+    //$np.addClass("open");
+
+    if($np.hasClass('open')){
+    	$np.removeClass('open');
+    }else {
+    	$np.addClass('open');
+    }
+
+  });
+
+  // if the nav is open, close it when the body is clicked
+  $("body").click(function() {
+    if ($("#nav-page").hasClass("open"))
+      $("#nav-page").removeClass('open');
+  });
+
+  // but don't close it when the nav itself is clicked
+  $("#nav-page").click(function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  });
