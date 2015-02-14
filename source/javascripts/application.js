6f3664ea49b5c114df150e4185004cfef17c65c8
davieghost <davidjohnson1297@gmail.com>
2015-02-13 20:43:44 -0800

    uhhhhhh

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -67,6 +67,14 @@ $(document).ready(function() {
     }
   });
 
+  $(window).scroll(function(){
+    if ($(window).scrollTop() > 100 ) {
+      $('#header').css('display', 'none');
+    } else {
+      $('#header').css('display', 'block');
+    }
+  });
+
   /////////////////// BACKGROUND VIDEO ///////////////
 
   // resize the video on document ready and window resize
